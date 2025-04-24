2f002de03748360d332684e14ef17ed163440534
Corey Brown <corey.brown.ny@gmail.com>
2025-04-23 20:49:10 -0400

    Add DB connection string to appsettings.json and inject DbContext into the application.

--- BlogMVC.Web/Program.cs
+++ BlogMVC.Web/Program.cs
@@ -1,7 +1,13 @@
+using BlogMVC.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BlogMVCDbContext>(optoins =>
+    optoins.UseSqlServer(builder.Configuration.GetConnectionString("BlogMVCDbConnectionString")));
 
 var app = builder.Build();
 
