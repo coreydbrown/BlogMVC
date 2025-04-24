212982b238d0d151136dbc6eaff61b189539089a
Corey Brown <corey.brown.ny@gmail.com>
2025-04-23 20:47:39 -0400

    Create DBContext class.

---
+++ BlogMVC.Web/Data/BlogMVCDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogMVC.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMVC.Web.Data
+{
+    public class BlogMVCDbContext: DbContext
+    {
+        public BlogMVCDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
