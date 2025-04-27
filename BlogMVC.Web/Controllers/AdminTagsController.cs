40c933fc18602d1f862357150606f7664363ad55
Corey Brown <corey.brown.ny@gmail.com>
2025-04-27 11:28:32 -0400

    Save new tags to database.

--- BlogMVC.Web/Controllers/AdminTagsController.cs
+++ BlogMVC.Web/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
-﻿using BlogMVC.Web.Models.ViewModels;
+﻿using BlogMVC.Web.Data;
+using BlogMVC.Web.Models.Domain;
+using BlogMVC.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogMVC.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BlogMVCDbContext blogMVCDbContext;
+
+        public AdminTagsController(BlogMVCDbContext blogMVCDbContext)
+        {
+            this.blogMVCDbContext = blogMVCDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -14,8 +23,14 @@ namespace BlogMVC.Web.Controllers
         [HttpPost]
         public IActionResult Add(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName = addTagRequest.DisplayName;
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            blogMVCDbContext.Tags.Add(tag);
+            blogMVCDbContext.SaveChanges();
             
             return View("Add");
         }
