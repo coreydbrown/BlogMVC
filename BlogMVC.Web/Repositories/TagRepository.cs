4cad4a254718f73300aa2d26503a897bf4586b58
Corey Brown <corey.brown.ny@gmail.com>
2025-05-03 17:22:06 -0400

    Create tag repository interface and implementation for repository pattern

---
+++ BlogMVC.Web/Repositories/TagRepository.cs
@@ -0,0 +1,65 @@
+﻿using BlogMVC.Web.Data;
+using BlogMVC.Web.Models.Domain;
+using BlogMVC.Web.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMVC.Web.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly BlogMVCDbContext blogMVCDbContext;
+
+        public TagRepository(BlogMVCDbContext blogMVCDbContext)
+        {
+            this.blogMVCDbContext = blogMVCDbContext;
+        }
+
+        public async Task<Tag> AddAsync(Tag tag)
+        {
+            await blogMVCDbContext.Tags.AddAsync(tag);
+            await blogMVCDbContext.SaveChangesAsync();
+            return tag;
+        }
+
+        public async Task<Tag?> DeleteAsync(Guid id)
+        {
+            var existingTag = await blogMVCDbContext.Tags.FindAsync(id);
+
+            if (existingTag != null)
+            {
+                blogMVCDbContext.Tags.Remove(existingTag);
+                await blogMVCDbContext.SaveChangesAsync();
+                return existingTag;
+            }
+
+            return null;
+        }
+
+        public async Task<IEnumerable<Tag>> GetAllAsync()
+        {
+            return await blogMVCDbContext.Tags.ToListAsync();
+        }
+
+        public async Task<Tag?> GetAsync(Guid id)
+        {
+            return await blogMVCDbContext.Tags.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Tag?> UpdateAsync(Tag tag)
+        {
+            var existingTag = await blogMVCDbContext.Tags.FindAsync(tag.Id);
+
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                await blogMVCDbContext.SaveChangesAsync();
+
+                return existingTag;
+            }
+
+            return null;
+        }
+    }
+}
