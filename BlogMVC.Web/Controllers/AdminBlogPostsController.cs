115ab3433490c3e8e9ed78fb4ae55e20176579f5
Corey Brown <corey.brown.ny@gmail.com>
2025-05-05 17:14:34 -0400

    Create controller for Blog Posts and View for adding a blog post

---
+++ BlogMVC.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMVC.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
