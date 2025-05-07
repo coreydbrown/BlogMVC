e63f853cd26e352271d38b39f7dd579e65142173
Corey Brown <corey.brown.ny@gmail.com>
2025-05-07 16:51:45 -0400

    Create interface for Blog Post repository to communicate with DB.

---
+++ BlogMVC.Web/Repositories/IBlogPostRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogMVC.Web.Models.Domain;
+
+namespace BlogMVC.Web.Repositories
+{
+    public interface IBlogPostRepository
+    {
+        Task<IEnumerable<BlogPost>> GetAllAsync();
+
+        Task<BlogPost?> GetAsync(Guid id);
+
+        Task<BlogPost> AddAsync(BlogPost blogPost);
+
+        Task<BlogPost?> UpdateAsync(BlogPost blogPost);
+
+        Task<BlogPost?> DeleteAsync(Guid id);
+    }
+}
