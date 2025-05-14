22fce0ce8b168786e00c2a1285b893c217580cd1
Corey Brown <corey.brown.ny@gmail.com>
2025-05-14 17:54:35 -0400

    Create page to edit a Blog Post and populate it with current data.

--- BlogMVC.Web/Controllers/AdminBlogPostsController.cs
+++ BlogMVC.Web/Controllers/AdminBlogPostsController.cs
@@ -1,4 +1,5 @@
-﻿using BlogMVC.Web.Models.Domain;
+﻿using System.Reflection.Metadata.Ecma335;
+using BlogMVC.Web.Models.Domain;
 using BlogMVC.Web.Models.ViewModels;
 using BlogMVC.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -72,6 +73,40 @@ namespace BlogMVC.Web.Controllers
             var blogPosts = await blogPostRepository.GetAllAsync();
 
             return View(blogPosts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var blogPost = await blogPostRepository.GetAsync(id);
+            var tagsDomainModel = await tagRepository.GetAllAsync();
+
+            if (blogPost != null)
+            {
+                var model = new EditBlogPostRequest
+                {
+                    Id = blogPost.Id,
+                    Heading = blogPost.Heading,
+                    PageTitle = blogPost.PageTitle,
+                    Content = blogPost.Content,
+                    Author = blogPost.Author,
+                    FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                    UrlHandle = blogPost.UrlHandle,
+                    ShortDescription = blogPost.ShortDescription,
+                    PublishedDate = blogPost.PublishedDate,
+                    Visible = blogPost.Visible,
+                    Tags = tagsDomainModel.Select(x => new SelectListItem
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }),
+                    SelectedTags = blogPost.Tags.Select(x => x.Id.ToString()).ToArray()
+                };
+
+                return View(model);
+            }
+
+            return View(null);
         }
     }
 }
--- BlogMVC.Web/Repositories/BlogPostRepository.cs
+++ BlogMVC.Web/Repositories/BlogPostRepository.cs
@@ -30,9 +30,9 @@ namespace BlogMVC.Web.Repositories
             return await blogMVCDbContext.BlogPosts.Include(x => x.Tags).ToListAsync();
         }
 
-        public Task<BlogPost?> GetAsync(Guid id)
+        public async Task<BlogPost?> GetAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await blogMVCDbContext.BlogPosts.Include(x => x.Tags).FirstOrDefaultAsync(x => x.Id == id);
         }
 
         public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
